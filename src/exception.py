11d48280e589a083d04f95ccac71587e2c486ae5
chirag6521 <chiragsingh38988@gmail.com>
2023-04-05 12:23:05 +0530

    EDA Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,10 +1,10 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python sxript name [{0}] line number [{1}] error message[{2}].format()"
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}].format()"
     file_name,exc_tb.tb_lineno,str(error)
 
     return error_message
@@ -12,18 +12,11 @@ def error_message_details(error,error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self,error_mesage,error_detail:sys):
-        super.__init__(error_mesage)
+        super().__init__(error_mesage)
         self.error_message=error_message_details(error_message_details,error_detail=error_detail)
 
     def __str__(self):
-        return self.error_message 
-    
-if __name__=="__main__":
+        return self.error_messag
 
-    try:
-        a=1/0
-    except Exception as e:
-        
-        logging.info("Divide by Zero ")
-        raise CustomException(e,sys)
-    
+
+
